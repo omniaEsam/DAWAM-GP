f8f119c21479f5e78f86f6a2e620329f3c628c37
DoaaAmin99 <dondon2799@gmail.com>
2023-07-04 03:21:35 +0300

    updateDesignOnCards

--- src/pages/AdvancedSearch/AdvancedSearch.jsx
+++ src/pages/AdvancedSearch/AdvancedSearch.jsx
@@ -20,14 +20,14 @@ export default function AdvancedSearch() {
  const fetchResult = searchResults.map((item)=>{
   return(
     <div className='col-md-12' key={item.id}>
-    <div className='waqf-cards  mt-4 d-flex '>
+    <div className='waqf-cards  mt-4 d-flex rounded shadow '>
       <div className='ms-4 w-25'>
       <img src={"http://afdinc-001-site5.itempurl.com"+item.imageUrl} className='w-100 ' alt=""/>
       </div>
       <div className='ps-5' >
       <h3 className='pt-4 pb-2 fs-5 fw-bold'>  {item.waqfName} </h3>
       <p className='desc-search fs-5'>{item.waqfDescription.split('').slice(0 , 105).join('')}</p>
-      <Link className="btn btn-green mt-3" to={`/ResultSearch/${item.id}`} role="button">المزيد</Link>              
+      <Link className="btn btn-green m-3" to={`/ResultSearch/${item.id}`} role="button">المزيد</Link>              
       </div>
     </div>
   </div>
